feea8a093c5941396f3821bdf89a29feb21b067b
madoka20 <fumx17@juniata.edu>
2020-03-22 18:04:30 +0800

    upload form

--- app/src/main/java/com/example/climate/UpdateForm.java
+++ app/src/main/java/com/example/climate/UpdateForm.java
@@ -14,10 +14,16 @@ public class UpdateForm extends AppCompatActivity {
         setContentView(R.layout.activity_update_form);
 
         //type selector
-        Spinner dropdown = findViewById(R.id.type_selector);
-        String[] items = new String[]{"Select type...","Snow storm", "Earthquake", "Volcano eruption"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown.setAdapter(adapter);
+        Spinner dropdown1 = findViewById(R.id.type_selector);
+        String[] items1 = new String[]{"Select type...","Snow storm", "Earthquake", "Volcano eruption"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items1);
+        dropdown1.setAdapter(adapter1);
+
+        //danger level selector
+        Spinner dropdown2 = findViewById(R.id.danger_level_selector);
+        String[] items2 = new String[]{"Select danger level...","1", "2", "3","4","5"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items2);
+        dropdown2.setAdapter(adapter2);
     }
 
 }
