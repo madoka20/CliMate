be46ac269e0fd82ce6ebf0e3d69bdbaea525851f
madoka20 <fumx17@juniata.edu>
2020-03-27 13:19:07 +0800

    can upload data to firebase

--- app/src/main/java/com/example/climate/UpdateForm.java
+++ app/src/main/java/com/example/climate/UpdateForm.java
@@ -1,21 +1,25 @@
 package com.example.climate;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -26,32 +30,31 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import android.content.Context;
-import android.widget.Toast;
 
 import java.io.File;
-import java.sql.Time;
 import java.sql.Timestamp;
 
-import static java.lang.Integer.parseInt;
-import static java.lang.Integer.valueOf;
-
 public class UpdateForm extends AppCompatActivity {
-    private FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-    private DatabaseReference mDatabaseReference = mDatabase.getReference();
-    private int userid;
-    private int eventid;
-    private String type;
+    public FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    public DatabaseReference mDatabaseReference = mDatabase.getReference();
+    public int userid=1;
+    public String eventId="2";
+    public String type;
 
-    private Timestamp time;
-    private int dangerlevel=1;
-    private int reliabilitylevel=1;
-    private String description;
-    private File evidence;
-    private TextView latTextView;
-    private TextView lonTextView;
+    public Timestamp time;
+    public int dangerlevel=1;
+
+
+    public TextView latTextView;
+    public TextView lonTextView;
+    public double lat;
+    public double lon;
+    public String description;
+    public TextView destv;
     int PERMISSION_ID = 44;
     FusedLocationProviderClient mFusedLocationClient;
+    Spinner dropdown1;
+    Spinner dropdown2;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,28 +66,57 @@ public class UpdateForm extends AppCompatActivity {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         getLastLocation();
 
+
+
+        //time
+        time=new Timestamp(System.currentTimeMillis());
         //type selector
-        Spinner dropdown1 = findViewById(R.id.type_selector);
+        dropdown1 = findViewById(R.id.type_selector);
         String[] items1 = new String[]{"Select type...","Snow storm", "Earthquake", "Volcano eruption"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items1);
         dropdown1.setAdapter(adapter1);
-        type = dropdown1.getSelectedItem().toString();
-
-
-        time=new Timestamp(System.currentTimeMillis());
 
         //danger level selector
-        Spinner dropdown2 = findViewById(R.id.danger_level_selector);
-        String[] items2 = new String[]{"Select danger level...","1", "2", "3","4","5"};
+        dropdown2 = findViewById(R.id.danger_level_selector);
+        String[] items2 = new String[]{"1", "2", "3","4","5"};
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items2);
         dropdown2.setAdapter(adapter2);
-//        dangerlevel=Integer.parseInt(dropdown2.getSelectedItem().toString()) ;
+
+        destv=findViewById(R.id.description);
     }
+
+    public void upload_event(View v){
+
+
+        type=dropdown1.getSelectedItem().toString();
+
+        dangerlevel=Integer.parseInt(dropdown2.getSelectedItem().toString()) ;
+
+        description=destv.getText().toString();
+
+        Events event=new Events(userid,type,lat,lon,time,dangerlevel,description);
+        mDatabaseReference.child("Events").child(eventId).setValue(event);
+
+        Intent myIntent = new Intent(this,MapsActivity.class);
+        startActivity(myIntent);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Update Successfully!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
 
     public void cancel(View v){
         Intent myIntent = new Intent(this,MapsActivity.class);
         startActivity(myIntent);
     }
+
+
+
 
 //get location functions
 @SuppressLint("MissingPermission")
@@ -101,6 +133,8 @@ private void getLastLocation(){
                             } else {
                                 latTextView.setText(location.getLatitude()+"");
                                 lonTextView.setText(location.getLongitude()+"");
+                                lat=location.getLatitude();
+                                lon=location.getLongitude();
                             }
                         }
                     }
